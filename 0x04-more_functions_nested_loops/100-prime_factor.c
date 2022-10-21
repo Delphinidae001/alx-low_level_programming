311145fbd1fa5dd711262af044402d752b0da09f
Delphinidae001 <djoshua57@gmail.com>
2022-10-20 17:59:28 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,10 +12,10 @@ int main(void)
 	unsigned long int i, n = 612852475143;
 
 	for (i = 3; i < 782849; i = i + 2)
-		{
-			while ((n % i == 0) && (n != i))
-				n = n / i;
-		}
-		printf("%lu\n", n);
-		return (0);
+	{
+		while ((n % i == 0) && (n != i))
+			n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
 }
